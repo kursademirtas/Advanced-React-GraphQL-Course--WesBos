d14aeec9b72d03180f74545d4a8d303be7029bca
Kursad Demirtas <69519700+kursademirtas@users.noreply.github.com>
2021-04-02 22:26:23 +0300

    navigation updated.

--- components/Nav.js
+++ components/Nav.js
@@ -1,16 +1,30 @@
-import React from 'react'
-import Link from 'next/link'
-import NavStyles from './styles/NavStyles'
+import React from "react";
+import Link from "next/link";
+import NavStyles from "./styles/NavStyles";
+import { useUser } from "./User";
+import SignOut from "./SignOut";
 
 const Nav = () => {
-	return (
-		<NavStyles>
-			<Link href="/products">Products</Link>
-			<Link href="/sell">Sell</Link>
-			<Link href="/orders">Orders</Link>
-			<Link href="/account">Account</Link>
-		</NavStyles>
-	)
-}
+  const user = useUser();
 
-export default Nav
+  return (
+    <NavStyles>
+      <Link href="/products">Products</Link>
+      {user && (
+        <>
+          <Link href="/sell">Sell</Link>
+          <Link href="/orders">Orders</Link>
+          <Link href="/account">Account</Link>
+          <SignOut />
+        </>
+      )}
+	  {!user && (
+		  <Link href="/signin">Sign In</Link>
+	  )
+
+	  }
+    </NavStyles>
+  );
+};
+
+export default Nav;
