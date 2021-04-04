d55b14bbe38da3cfd18b426b7bbe33d36ce5e784
Kursad Demirtas <69519700+kursademirtas@users.noreply.github.com>
2021-04-04 23:26:58 +0300

    sign page updated.

--- pages/signin.js
+++ pages/signin.js
@@ -1,12 +1,22 @@
 import SignIn from "../components/SignIn";
+import SignUp from "../components/SignUp";
+import styled from 'styled-components';
+import RequestReset from "../components/RequestReset";
 
 
+const Grid = styled.div`
+	display:grid;
+	grid-template-columns:repeat(auto-fit, minmax(300px,1fr));
+	grid-gap:2rem;
+` 
 
 function SignInPage() {
 	return (
-		<div>
+		<Grid>
 			<SignIn />
-		</div>
+			<SignUp />
+			<RequestReset />
+		</Grid>
 	)
 }
 
